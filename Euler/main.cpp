9925d07adb0f84d6b87f4e52892b4b56e7e9c437
Yonis Voloshyn <b.voloshyn@gmail.com>
2018-12-20 21:34:30 +0200

    Add Euler Project

---
+++ Euler/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "tasks25.h"
+
+using namespace std;
+
+int main()
+	{
+	//cout << fibonachi_even ()<< endl;
+	//factor ();
+	return 0;
+	}
---
+++ Euler/tasks25.h
@@ -0,0 +1,12 @@
+﻿#ifndef TASKS25_H
+#define TASKS25_H
+#include <iostream>
+//#include <cstdlib>
+//#include <cstdio>
+
+size_t fibonachi_even();
+void factor();
+size_t primul(size_t);
+void palindrom();
+
+#endif // TASKS25_H
